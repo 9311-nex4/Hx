959fc4860276924d36a56d445c908c59420c5278
9311-nex4 <spaceherogaming007@gmail.com>
2026-04-27 01:01:37 +0700

    Update main_v3.lua

--- Main_Script/main_v3.lua
+++ Main_Script/main_v3.lua
@@ -5,6 +5,7 @@ local NguoiChoi        = Players.LocalPlayer
 local PlayerGui        = NguoiChoi:WaitForChild("PlayerGui")
 local PlayerScripts    = NguoiChoi:WaitForChild("PlayerScripts")
 
+
 local ThuMucUI
 local KiemTraHui, KetQuaHui = pcall(function() return gethui() end)
 local KiemTraCoreGui, KetQuaCoreGui = pcall(function() return game:GetService("CoreGui") end)
